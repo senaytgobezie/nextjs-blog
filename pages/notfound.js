36fb5152eab3b7c4a992a76d2f3b7ba2cd87e132
Senayt Gobezie <senaitgobezie@gmail.com>
2023-03-28 10:46:33 +0300

    next-js

---
+++ pages/notfound.js
@@ -0,0 +1,11 @@
+// export  default function Custom404(){
+//     return(
+//         <h1>the page is not found</h1>
+//     )
+// }
+ const Custom404=()=>{
+    return(
+        <h1>the pade can't be found</h1>
+    )
+ }
+ export default Custom404;
